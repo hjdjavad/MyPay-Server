36cff14baca81bc2f00e197a8c1d0632741f9f04
Javad Dadkhah <hjd1831da@gmail.com>
2021-01-03 13:40:21 +0330

    init origin

---
+++ MyPay.UI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyPay.UI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public async Task<string> Post([FromBody] string value)
+        {
+            return null;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<string> Put(int id, [FromBody] string value)
+        {
+            return null;
+        }
+        [HttpDelete("{id}")]
+        public async Task<string> Delete(int id)
+        {
+            return null;
+        }
+    }
+}
