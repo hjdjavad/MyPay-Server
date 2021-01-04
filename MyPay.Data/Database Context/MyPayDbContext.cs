748688f95453ddc5bca86d94778173034b0eef6f
Javad Dadkhah <hjd1831da@gmail.com>
2021-01-05 02:16:27 +0330

    init origin

---
+++ MyPay.Data/Database Context/MyPayDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyPay.Data.Database_Context
+{
+    class MyPayDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=.;Initial Catalog=MyPayDB;Integrated Security=True;MultipleActiveResultSets=True;");
+        }
+    }
+}
---
+++ MyPay.Data/Models/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyPay.Data.Models
+{
+    public class BaseEntity<T>
+    {
+        [Key] 
+        public T Id { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+        [Required]
+        public DateTime DateModified { get; set; }
+    }
+}
